440aaf5b41ad6590d999811e8dbb063942dd6a1a
sahil-viradiya <sahilviradiya999@gmail.com>
2022-09-21 15:19:54 -0700

    C Language

--- string/strrev.c
+++ string/strrev.c
@@ -1,14 +1,29 @@
-#include<stdio.h>
-#include<string.h>
+#include <stdio.h>
+#include <string.h>
 
+// void main()
+// {
+//     char ch[50];
+
+//     printf("Enter Your Name & Surname :");
+//     gets(ch);
+
+//    printf("your name & surname is : %s \n",strrev(ch));
+
+// }
+// without using function
 void main()
 {
     char ch[50];
+    int length;
 
-    printf("Enter Your Name & Surname :");
+    printf("Please insert the string you want to revers : \n");
     gets(ch);
 
-   printf("your name & surname is : %s \n",strrev(ch));
+    length = strlen(ch);
 
-   
+    for (int i = length; i >= 0; i--)
+    {
+        printf("%c", ch[i]);
+    }
 }
