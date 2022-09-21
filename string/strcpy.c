55594d27d926451d98a37574ba7885dffaa8d5d9
sahil-viradiya <sahilviradiya999@gmail.com>
2022-09-21 16:37:46 -0700

    C Language

--- string/strcpy.c
+++ string/strcpy.c
@@ -1,15 +1,31 @@
-#include<stdio.h>
-#include<string.h>
+#include <stdio.h>
+#include <string.h>
+
+// void main()
+// {
+//     char ch[50],st[50];
+
+//     printf("Enter Your Name & Surname :");
+//     gets(ch);
+
+//    printf("your name & surname is : %s \n",ch);
+
+//    strcpy(st,ch);
+//    printf("Copy String is : %s",st);
+// }
 
 void main()
 {
-    char ch[50],st[50];
+    char ch[50], cp[50], i;
 
-    printf("Enter Your Name & Surname :");
+    printf("Enter A String :");
     gets(ch);
 
-   printf("your name & surname is : %s \n",ch);
+    for (i = 0; ch[i]>='\0'; i++)
+    {
+        cp[i] = ch[i];
+    }
+    printf("first string is :%s\n ", ch);
 
-   strcpy(st,ch);
-   printf("Copy String is : %s",st);
+    printf("Copy String is :%s", cp);
 }
