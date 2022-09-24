7c781161dd6d0426590f0e003fd2337bf4a90fb3
sahil-viradiya <sahilviradiya999@gmail.com>
2022-09-23 17:10:14 -0700

    C Language

--- Array/Einsert.c
+++ Array/Einsert.c
@@ -1,19 +1,63 @@
+// #include <stdio.h>
+// #include <string.h>
+// void main()
+// {
+//     int a[50] = {1, 2, 3, 4, 5};
+//     int b[50] = {};
+//     int n = 9;
+//     int position = 2;
+
+//     int j = 0;
+//     for (int i = 0; i < 6; i++)
+//     {
+
+//         if (i == position)
+//         {
+//             b[i] = n;
+//         }
+//         else
+//         {
+//             b[i] = a[j];
+//             j++;
+//         }
+//     }
+
+//     for (int i = 0; i < 6; i++)
+//     {
+//         printf("%d", b[i]);
+//     }
+// }
+
 #include <stdio.h>
-#include <string.h>
 void main()
 {
-    int a[50] = {1, 2, 3, 4, 5};
-    int b[50] = {};
-    int n = 9;
-    int position = 2;
+    int n, i, a[50], pos, e, b[50];
+    // size of array
+    printf("Enter size of array :");
+    scanf("%d", &n);
 
-    int j = 0;
-    for (int i = 0; i < 6; i++)
+    printf("\n Enter Element of array :");
+    for (i = 0; i < n; i++)
     {
+        scanf("%d", &a[i]);
+    }
 
-        if (i == position)
+    // enter postion
+    printf("\n Enter Postion :");
+    scanf("%d", &pos);
+
+    // enter instert Element
+    printf("\n Enter New Element :");
+    scanf("%d", &e);
+
+    // print array
+    int j = 0;
+    for (i = 0; i < n + 1; i++)
+    {
+        // printf("%d,", a[i]);
+        if (a[i] == pos)
         {
-            b[i] = n;
+            b[i] = e;
         }
         else
         {
@@ -21,9 +65,8 @@ void main()
             j++;
         }
     }
-
-    for (int i = 0; i < 6; i++)
+    for (i = 0; i < n; i++)
     {
-        printf("%d", b[i]);
+        printf("\n%d,", b[i]);
     }
 }
